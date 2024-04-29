870810d65f487e732920366053812819f503b09b
tmytcbn <tmytcbn@ymail.ne.jp>
2024-04-30 08:43:36 +0900

    Lesson18 Chapter4.5 プログラムの実行

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user(name, gender, age, email)
+    values("キラメキ太郎", "男性", 27, 'taro.kirameki@mail.com');
+insert into user(name, gender, age, email)
+    values("キラメキ次郎", "男性", 22, 'jiro.kirameki@mail.com');
+insert into user(name, gender, age, email)
+    values("キラメキ花子", "女性", 25, 'hanako.kirameki@mail.com');
