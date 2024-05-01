da63ebc2ebc55cc8ad2288a61729e989b5995bac
tmytcbn <tmytcbn@ymail.ne.jp>
2024-05-02 07:56:00 +0900

    Lesson18 Chapter7.6 プログラムの実行

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -3,6 +3,7 @@ package com.techacademy.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -19,6 +20,12 @@ public class UserService {
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 
 }
