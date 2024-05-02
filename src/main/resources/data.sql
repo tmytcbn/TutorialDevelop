f17f3b56a6fd820f7e2814598ff7291c782a25fe
tmytcbn <tmytcbn@ymail.ne.jp>
2024-05-02 12:41:18 +0900

    Lesson18 Chapter11.10 プログラムの実行

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,5 @@ insert into user(name, gender, age, email)
     values("キラメキ次郎", "男性", 22, 'jiro.kirameki@mail.com');
 insert into user(name, gender, age, email)
     values("キラメキ花子", "女性", 25, 'hanako.kirameki@mail.com');
+insert into authentication(login_user, password, valid_date, user_id)
+    values("ktaro", "$2a$08$clh9XaYYznpX9WDqySgiCuUu4znpSeu2oJi5l2Q00UJs42Llrbd7S", "9999-12-31", 1);
