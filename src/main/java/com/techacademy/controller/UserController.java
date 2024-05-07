58b39754cda3a31dcf4339f62a6855ca1ef4b5c2
tmytcbn <tmytcbn@ymail.ne.jp>
2024-05-07 16:27:01 +0900

    課題質問

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -56,16 +56,27 @@ public class UserController {
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
+    public String getUser(@PathVariable("id") Integer id, User user, Model model) {
+        // idがnullじゃなかったら
+        if(id != null) {
+            // idから検索した情報をModelに登録
+            model.addAttribute("user", service.getUser(id));
+        // idがnullなら
+        } else {
+            // 入力していた情報をModelに登録
+            model.addAttribute("user", user);
+        }
         // User更新画面に遷移
         return "user/update";
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@PathVariable("id") Integer id, User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getUser(null, user, model);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
